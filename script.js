ca8fbd132f093aa5edb8807d1ea9cb508bfbbcb3
finnhodgkin <finnhodgkin@gmail.com>
2017-01-19 16:30:26 +0000

    clean up obsolete code...

--- script.js
+++ script.js
@@ -1,70 +1,70 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var rules = {
+document.addEventListener('DOMContentLoaded', function() {
+  const rules = {
     numberOfBlocks:25, //width of the platform - 20px increments
     direction: false,   //true = right, false = left
     currentBlock: 1
+  };
 
-  }
-
-  genBlocks = (num,size)=>{
-    var blocks = []
-    for(i = 1; i < num+1; i++){
-      s = size ? " style='width:" + size + "px;' " : "";
-      blocks.push("<div id='block" + i + "' class='block'" + s + "></div>")
+  function genBlocks(num, size) {
+    let blocks = [];
+    for (let i = 1; i < num + 1; i++) {
+      let s = size ? ` style='width:${size}px;' ` : '';
+      blocks.push(`<div id='block${i}' class='block'${s}></div>`);
     }
-    return blocks.join("")
+    return blocks.join('');
   }
 
-  var wrap = document.getElementById("wrap");
-  wrap.innerHTML = genBlocks(25) + "\n" + wrap.innerHTML;
+  const wrap = document.getElementById('wrap');
+  wrap.innerHTML = `${genBlocks(rules.numberOfBlocks)}\n${wrap.innerHTML}`;
 
   //spacebar to change direction
-  document.addEventListener("keydown", function(event) {
-    if(event.which === 32){
-      if(rules.direction){
-        document.getElementById('player').style.left = "0px";
+  document.addEventListener('keydown', (event) => {
+    if (event.which === 32) {
+      if (rules.direction) {
+        document.getElementById('player').style.left = '0px';
         rules.direction = false;
       }
-      else{
-        document.getElementById('player').style.left = "480px";
+      else {
+        document.getElementById('player').style.left = '480px';
         rules.direction = true;
       }
     }
-  })
+  });
 
   //listen for player
-  var playerControl = setInterval(function () {
+  var playerControl = setInterval(() => {
     let current = Math.round(Math.ceil(
                   getComputedStyle(document.getElementById('player'))
-                  .getPropertyValue("left")
+                  .getPropertyValue('left')
                   .slice(0,-2)) / 20) + 1;
 
     //recolour previous block
-    if(current !== rules.currentBlock)
-      document.getElementById('block' + rules.currentBlock)
-      .style.backgroundColor = "black";
+    if (current !== rules.currentBlock) {
+      document.getElementById(`block${rules.currentBlock}`).style.backgroundColor = 'black';
+    }
 
     //colour and set current block
     rules.currentBlock = current;
-    document.getElementById('block' + rules.currentBlock)
-      .style.backgroundColor = "red";
+    document.getElementById(`block${rules.currentBlock}`)
+      .style.backgroundColor = 'red';
   }, 40);
 
   //click for killer block
-  var blocks = document.getElementsByClassName('block');
-  for(let i = 0; i < blocks.length; i++){
-    blocks[i].addEventListener('click',function(event){
+  const blocks = document.getElementsByClassName('block');
+  blocks.forEach((ele) => {
+    ele.addEventListener('click', function () {
       blink(this);
-    })
-  }
+    });
+  });
+});
 
 
-  function blink (obj) {
-    setInterval(function () {
-      if(obj.style.backgroundColor === "black")
-        obj.style.backgroundColor = "blue";
-      else
-        obj.style.backgroundColor = "black";
-    }, 1000);
-  }
-})
+function blink (obj) {
+  setInterval(function () {
+    if (obj.style.backgroundColor === 'black') {
+      obj.style.backgroundColor = 'blue';
+    } else {
+      obj.style.backgroundColor = 'black';
+    }
+  }, 1000);
+}
